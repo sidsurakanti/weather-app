2ac242c91088d0ab84f5c799403a73b75442893d
AkIonSight <akionsight@gmail.com>
2020-09-08 11:38:39 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import weather as ws
+import sys
 
 city = input("City: ")
+try:
+    key = sys.argv[1]
+except IndexError:
+    key = input("enter your API key: ")
 
-temperature, humidity = ws.get(city)
+temperature, temperature_celcius, humidity = ws.get(city, key)
 print(f"\nWeather in {city.title()}:")
-print(f"Temperature: {temperature}")
+print(f"Temperature (in farenheight): {temperature}")
+print(f"Temperature (in celcius): {temperature_celcius}")
 print(f"Humidity: {humidity}")
 
 
